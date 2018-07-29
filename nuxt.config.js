73888f9905b14f9a1241d86cf8c9e5bfe43fa12e
Alexey Stratan <alexey.stratan@gmail.com>
2018-07-29 10:19:26 +0300

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,16 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
   head: {
     title: 'cv-nuxt',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Customize the progress bar color
-  */
   loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
   build: {
-    /*
-    ** Run ESLint on save
-    */
     extend (config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
@@ -34,6 +21,13 @@ module.exports = {
         })
       }
     }
+  },
+  router: {
+    middleware: 'i18n'
+  },
+  plugins: ['~/plugins/i18n.js'],
+  generate: {
+    routes: ['/', '/en']
   }
 }
 
